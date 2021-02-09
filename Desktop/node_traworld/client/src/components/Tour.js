4c5fa6196c5083e4611cf7133695056ded182191
yumi <>
2021-02-09 18:07:44 +0900

    update Tour page

--- Desktop/node_traworld/client/src/components/Tour.js
+++ Desktop/node_traworld/client/src/components/Tour.js
@@ -1,44 +1,46 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
+import TourCard from './TourCard';
 import imgCanada from '../images/img_canada.jpg';
-import imgParis from '../images/img_paris.jpg';
-import imgRome from '../images/img_rome.jpg';
+import imgFrance from '../images/img_paris.jpg';
+import imgItaly from '../images/img_rome.jpg';
 import imgSingapore from '../images/img_singapore.jpg';
-import imgTaipei from '../images/img_taipei.jpg';
+import imgTaiwan from '../images/img_taipei.jpg';
 import imgUsa from '../images/img_usa.jpg';
-import { GridList } from '@material-ui/core';
 
 const TourBlock = styled.div`
     display: flex;
-    width: 100%;
-    height: 100vh;
-    align-items: center;
     background: lightgray;
-    padding: 100px;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
 `;
 
-const openImageViewr = () => {
+const TourContent = styled.div`
+    padding: 5rem;
+`;
 
-}
+const CardBlock = styled.div`
+    display: flex; 
+`;
 
 function Tour() {
-    const images = [imgCanada, imgParis, imgRome, imgSingapore, imgTaipei, imgUsa]
     return (
         <TourBlock>
-            <GridList cols={3} spacing={15}>
-                {images.map((src, index) => (
-                    <img
-                        src={src}
-                        onClick={() => openImageViewr(index)}
-                        width="400px"
-                        height="260px"
-                        key={index}
-                        alt=""
-                    />
-                ))}
-            </GridList>
+            <TourContent>
+                <CardBlock>
+                    <TourCard img={imgUsa} country="미국(USA)" detail="lorem"></TourCard>
+                    <TourCard img={imgCanada} country="캐나다(Canada)" detail="lorem"></TourCard>
+                    <TourCard img={imgFrance} country="프랑스(France)" detail="lorem"></TourCard>
+                </CardBlock>
+                <CardBlock>
+                    <TourCard img={imgItaly} country="이탈리아(Italy)" detail="lorem"></TourCard>
+                    <TourCard img={imgSingapore} country="싱가폴(Singapore)" detail="lorem"></TourCard>
+                    <TourCard img={imgTaiwan} country="대만(Taiwan)" detail="lorem"></TourCard>
+                </CardBlock>
+            </TourContent>
         </TourBlock>
-    )
+    );
 }
 
 export default Tour
---
+++ Desktop/node_traworld/client/src/components/TourCard.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 300,
+        margin: 10,
+    },
+    media: {
+        width: 300,
+        height: 200,
+    },
+});
+
+const textCountry = {
+    fontSize: "1.5rem",
+}
+
+const textDetail = {
+    fontSize: "0.8rem",
+    color: "gray",
+}
+
+function TourCard({ img, country, detail }) {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={img}
+                    title={country}
+                />
+                <CardContent>
+                    <div style={textCountry}>{country}</div>
+                    <div style={textDetail}>{detail}</div>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">Reservation</Button>
+                <Button size="small" color="primary">More</Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default TourCard
